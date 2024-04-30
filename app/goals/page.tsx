6dbe9000248b7dd74462bf4a2e01f800613862c1
Chris Hong <hongchris97@gmail.com>
2024-04-29 23:37:03 -0700

    goals page

--- app/goals/page.tsx
+++ app/goals/page.tsx
@@ -11,7 +11,6 @@ export default function Goals() {
                         Add Goal
                     </button>
                 </div>
-
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     {/* Example Goal Card */}
                     <div className="bg-white shadow rounded-lg p-4">
