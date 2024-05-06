12a123691028f477296d2d20e0f6fb7096a5145f
Chris Hong <hongchris97@gmail.com>
2024-05-05 23:50:35 -0700

    styling

--- components/button/ButtonTemplate.tsx
+++ components/button/ButtonTemplate.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 interface ButtonTemplateProps {
     text: string;
     onClick: () => void;
+    btnStyle: string;
 }
 
-export default function ButtonTemplate({ text, onClick }: ButtonTemplateProps) {
+export default function ButtonTemplate({ text, onClick, btnStyle }: ButtonTemplateProps) {
   return (
-    <button onClick={onClick}>
+    <button onClick={onClick} className={btnStyle}>
       {text}
     </button>
   )
